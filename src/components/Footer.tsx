636890feaebe84495bf944792beef809b28cd83d
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-09-19 19:27:27 +0530

    Project Submission

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,6 @@ export default function Footer() {
                     <a href="https://twitter.com/ClassicDriveNFT" target="_blank" rel='noreferrer'><i className="fa-brands fa-twitter hover:scale-125 hover:text-gold_500 hover:cursor-pointer duration-300"></i></a>
                     <a href="https://discord.gg/77RgBU6wzS" target="_blank" rel='noreferrer'><i className="fa-brands fa-discord hover:scale-125 hover:text-gold_500 hover:cursor-pointer duration-300"></i></a>
                 </div>
-                <div className='text-gold_500 justify-center items-center space-y-5 md:justify-between flex flex-col md:flex-row text-2xl font-semibold'>
-                    <span>Provable fairness</span>
-                    <span>Terms & conditions</span>
-                </div>
             </footer>
         </main>
     )
